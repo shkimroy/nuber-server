6b581eacd59f5323436de43cd227676c4d02549a
shkim <shkim.roy.d@gmail.com>
2019-06-09 19:19:17 +0900

    Update EmailSignIn.resolvers.ts

--- src/api/User/EmailSignIn/EmailSignIn.resolvers.ts
+++ src/api/User/EmailSignIn/EmailSignIn.resolvers.ts
@@ -1,6 +1,7 @@
 import { Resolvers } from "../../../types/resolvers";
 import { EmailSignInMutationArgs, EmailSignInResponse } from "../../../types/graph";
 import User from "../../../entities/User";
+import createJWT from "../../../utils/createJWT";
 
 const resolvers: Resolvers = {
   Mutation: {
@@ -18,10 +19,11 @@ const resolvers: Resolvers = {
 
         const checkPassword = await user.comparePassword(password);
         if (checkPassword) {
+          const token = createJWT(user.id)
           return {
             ok: true,
             error: null,
-            token: "Comming soon"
+            token
           }
         } else {
           return {
